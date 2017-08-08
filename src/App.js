32490eeaddb766e3ca5267987291af23fa9f9d32
Suhail Idrees <gusmanbhai@hotmail.com>
2017-08-08 01:54:34 -0400

    Install react-modal, create test modal in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import ReactModal from 'react-modal';
+
 
 import { observer } from "mobx-react";
 
 @observer
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      showModal: false,
+    }
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+  
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+  
+  handleCloseModal () {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>{this.props.store.name}</h1>
+        
+        <button onClick={this.handleOpenModal}> TEST </button>
+
+        <ReactModal 
+           isOpen={this.state.showModal}
+           contentLabel="Minimal Modal Example"
+        >
+        <h3>TEST</h3>
+
+        <div>
+          <button 
+            onClick={this.handleCloseModal}>Cancel
+          </button>
+          
+        </div>
+        </ReactModal>
       </div>
     );
   }
