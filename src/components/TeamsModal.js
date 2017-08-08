a3352db1d9a1aaaa90e9a41d37f58714bac0a9fe
Suhail Idrees <gusmanbhai@hotmail.com>
2017-08-08 04:47:48 -0400

    Create component folder with a seperate modal for each category

---
+++ src/components/TeamsModal.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import ReactModal from 'react-modal';
+
+import { observer } from "mobx-react";
+
+
+@observer
+class TeamsModal extends Component {
+  
+  handleCloseModal () {
+    this.props.store.showTeamsModal = false;
+  }
+
+  handleModalChange(e){
+    this.props.store.tempTeams = e.target.value
+  }
+
+  handleSaveModal () {
+    let inputText = this.props.store.tempTeams;
+
+    this.props.store.teams = inputText;
+    this.props.store.showTeamsModal = false;
+  }
+
+  render(){
+    return (
+      <div>
+        <ReactModal 
+           isOpen={this.props.store.showTeamsModal}
+           contentLabel="Minimal Modal Example"
+           style={styles.modal}
+        >
+
+        <form>
+         Edit Teams <input type='text' onChange={this.handleModalChange.bind(this)}></input>
+        </form>
+
+        <div style={styles.buttonContainer}>
+          <button 
+            style={styles.modalButton} 
+            onClick={this.handleCloseModal.bind(this)}>Cancel
+          </button>
+          <button 
+            style={styles.modalButton} 
+            onClick={this.handleSaveModal.bind(this)}>Save
+          </button>
+        </div>
+
+        </ReactModal>
+      
+
+        
+        </div>
+      );
+  }
+};
+
+const styles = {
+  container: {
+    margin: 30
+  },
+  modal: {
+    content: {
+      color: 'black',
+      height: 250, 
+      width: 300,
+      borderRadius: 0,
+      marginLeft: '27%',
+      marginTop: '15%'
+    }
+  },
+  buttonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  editButton: {
+    height: 40,
+    width: 150,
+    borderRadius: 0,
+    fontSize: 13,
+  },
+  modalButton: {
+    height: 40, 
+    width: 200,
+    borderRadius: 0,
+    marginTop: '40%',
+  },
+  horizontalRule: {
+    marginTop: 20,
+    borderColor: 'grey',
+    borderWidth: 2
+  },
+};
+
+export default TeamsModal;
